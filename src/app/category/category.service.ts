77debe6b67945f6af59c0a510ab88e72c952b314
NasahK <nasahnash19@gmail.com>
2019-10-12 00:37:51 +0100

    Designed the header component, added some tags add styles to layout/homepage/main-component

--- src/app/category/category.service.ts
+++ src/app/category/category.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Category} from './category.model';
+import { Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
+export class ProductCategoryService {
 
-  constructor() { }
+  private url : string = "/assets/images/images.json";
+
+  constructor(private http: HttpClient) { }
+
+  //function gets all categories
+  getCategory():Observable<Category[]>
+  {
+    return this.http.get<Category[]>("https://product-api-gg-c.herokuapp.com/api/category");
+  }
 }
