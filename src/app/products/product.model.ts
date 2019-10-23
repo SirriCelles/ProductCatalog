2ac320354844d1b1e5827ceee0fced048c103d58
NasahK <nasahnash19@gmail.com>
2019-10-23 15:59:49 +0100

    Still so many challenges

--- src/app/products/product.model.ts
+++ src/app/products/product.model.ts
@@ -2,9 +2,11 @@ export class Product {
 
     category: {
         name: string;
+        id:number;
     }
     name:string;
     quantity: number;
     price: number;
     imageUrl: string;
+  
 }
