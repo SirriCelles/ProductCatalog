77b3cec7987db080a2dc74bd7e753eaec2f07300
SirriCelles <sirricelles@gmail.com>
2019-12-04 23:52:55 +0100

    Minor updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,18 +16,20 @@ export class AppComponent implements OnInit, OnDestroy{
 
   // }
 
-  constructor(private translate: TranslateService)
-  {  
-    // console.log(translate.);
+//   constructor(private translate: TranslateService)
+//   {  
+//     // console.log(translate.);
+  
+  
     
-    translate.addLangs(['en-English', 'fr-French','de-German', 'es-Spanish', 'zh-Chinese']);
-    //this language will be used as a fallback when a translation isn't found in the current language
-    translate.setDefaultLang('en-English');
+//     translate.addLangs(['en-English', 'fr-French','de-German', 'es-Spanish', 'zh-Chinese']);
+//     //this language will be used as a fallback when a translation isn't found in the current language
+//     translate.setDefaultLang('en-English');
 
-    // Changes the lang currently used
-      // the lang to use, if the lang isn't available, it will use the current loader to get them
-      translate.use('en-English');      
- }
+//     // Changes the lang currently used
+//       // the lang to use, if the lang isn't available, it will use the current loader to get them
+//       translate.use('en-English');      
+//  }
 
   ngOnInit(){
     
@@ -37,4 +39,6 @@ export class AppComponent implements OnInit, OnDestroy{
     
   }
 }
+
+
   
--- src/app/layout/header/search/search.component.ts
+++ src/app/layout/header/search/search.component.ts
@@ -1,3 +1,4 @@
+import { TranslateService } from '@ngx-translate/core';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
@@ -12,11 +13,18 @@ export class SearchComponent implements OnInit, OnChanges {
   // param = {value: 'this.componentName'};
   messageBoxContent = TRANSLATE('Login');
 
-  constructor(private route: ActivatedRoute) {
+   constructor(private route: ActivatedRoute, public translate: TranslateService)
+  {  
+    // console.log(translate.); 
     
-    
-    
-   }
+    translate.addLangs(['English', 'French','German', 'Spanish', 'Chinese']);
+    //this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('English');
+
+    // Changes the lang currently used
+      // the lang to use, if the lang isn't available, it will use the current loader to get them
+      translate.use('English');      
+ }
 
   
    ngOnChanges() {    
